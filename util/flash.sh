ccb39f6d6ba0940040c00fda5453e3c83fa7b50f
Peter Lavigne <peter.lavigne@appfolio.com>
2020-05-16 11:25:36 -0400

    auto push to github

--- util/flash.sh
+++ util/flash.sh
@@ -4,7 +4,13 @@ echo 'Have you downloaded the peter-lavigne.json file and reset your keyboard?'
 read -p '(y/n)' response
 
 if [ $response = "y" ]; then
-  qmk json2c -o /Users/peter.lavigne/qmk_firmware/keyboards/hotdox/keymaps/peter-lavigne/keymap.c ~/Downloads/peter-lavigne-keymap.json && qmk flash && mv ~/Downloads/peter-lavigne-keymap.json /Users/peter.lavigne/qmk_firmware/peter-lavigne-keymap.json
+  cd /Users/peter.lavigne/qmk_firmware
+  qmk json2c -o keyboards/hotdox/keymaps/peter-lavigne/keymap.c ~/Downloads/peter-lavigne-keymap.json
+  qmk flash
+  mv ~/Downloads/peter-lavigne-keymap.json /peter-lavigne-keymap.json
+  git add -A
+  git commit -m "update keyboard"
+  git push
 else
   echo "Exiting..."
 fi
