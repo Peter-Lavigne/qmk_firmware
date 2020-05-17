a8319701e8e58513817e9d43c87b4160f6c6633e
Peter Lavigne <peter.lavigne@appfolio.com>
2020-05-17 10:21:43 -0400

    update keyboard

--- util/flash.sh
+++ util/flash.sh
@@ -5,9 +5,13 @@ read -p '(y/n)' response
 
 if [ $response = "y" ]; then
   cd /Users/peter.lavigne/qmk_firmware
+  echo 'Converting json...'
   qmk json2c -o keyboards/hotdox/keymaps/peter-lavigne/keymap.c ~/Downloads/peter-lavigne-keymap.json
+  echo 'Flashing...'
   qmk flash
+  echo 'Cleaning up...'
   mv ~/Downloads/peter-lavigne-keymap.json peter-lavigne-keymap.json
+  echo 'Saving...'
   git add -A
   git commit -m "update keyboard"
   git push
